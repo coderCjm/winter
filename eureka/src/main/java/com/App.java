52f40c35ecf3a8aa72cce7a7ca3335b4bc889e5c
coderCjm <13774486042@163.com>
2018-12-13 14:02:32 +0800

    版本调换

--- eureka/src/main/java/com/App.java
+++ eureka/src/main/java/com/App.java
@@ -3,7 +3,6 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by sandog on 2018/11/9.
